1caef5ee4172dec3b9e2b97f712de5a2ea79ce74
varun <varunbharadwaj444@gmail.com>
2022-11-28 21:46:56 +0530

    initial commit

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,4 +1,4 @@
 import contract from './AVAXGods.json';
 
-export const ADDRESS = '0xC6825E381F728a0300f3FD1bf82d9B378FFD83eA';
+export const ADDRESS = '0xf3bF3674d7a016CcE28863cb1Fe2aF3B095041F1';
 export const { abi: ABI } = contract;
